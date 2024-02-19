9c4b1404bede283ce5be7e49961dde8e9ef3d2a0
Mateus Adamo <adamomateus478@gmail.com>
2024-02-19 18:55:55 -0300

    feat: Point created.

---
+++ src/components/PointRed.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+
+import tw from 'twrnc'
+
+
+export default function PointRed() {
+
+    return (
+        <View style={tw`bg-red-600 w-2 h-2 top-4/10 left-2 z-10 rounded-full`}></View>
+    )
+}
+
