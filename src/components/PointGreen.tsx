da221805dddde5038da08fbcbc5e27efe08f838c
Mateus Adamo <adamomateus478@gmail.com>
2024-02-23 18:19:07 -0300

    feat: Green Point for emplooyes active created.

---
+++ src/components/PointGreen.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+
+import tw from 'twrnc'
+
+
+export default function PointGreen() {
+
+    return (
+        <View style={tw`bg-green-600 w-2 h-2 top-4/10 left-2 z-10 rounded-full`}></View>
+    )
+}
+
