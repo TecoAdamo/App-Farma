ef649bafc20414471c063b95dc462459081bac9d
Mateus Adamo <adamomateus478@gmail.com>
2024-02-23 18:21:32 -0300

    feat: Button component for register users and emplooyes created.

---
+++ src/components/RegisterEmplooyes.tsx
@@ -0,0 +1,19 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+import tw from 'twrnc';
+
+export default function RegisterEmplooye() {
+    return (
+        <View style={tw` items-center top-1/8`}>
+
+            <View style={tw`top-1/8 right-1/8 p-1 w-full rounded gap-4`}>
+                <TouchableOpacity
+                    style={tw`flex-row items-center bg-gray-900 p-2 rounded gap-4`}>
+                    <Ionicons name="person-add-outline" size={32} color="white" />
+                    <Text style={tw`font-bold right-1/8 text-white`}>Cadastrar Usuários</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
