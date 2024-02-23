ea549b06f40705d1d9ebf9027259070d27f97a8e
Mateus Adamo <adamomateus478@gmail.com>
2024-02-23 18:20:58 -0300

    feat: Status for emplooyes added.

--- src/components/EmplooyeStatus.tsx
+++ src/components/EmplooyeStatus.tsx
@@ -1,19 +1,28 @@
 import { View, Text } from 'react-native';
 
 import tw from 'twrnc'
+
 import PointRed from './PointRed';
+import PointGreen from './PointGreen';
 
+type EmployeeStatusProps = {
+    isActive: boolean
+}
 
-export default function EmployeeStatus() {
-
+export default function EmployeeStatus({ isActive }: EmployeeStatusProps) {
     return (
         <View style={tw`w-full`}>
-
-            <PointRed />
-            <Text style={tw`font-bold mb-2 bg-red-200 border-red-600 border-2 p-1 w-4/8 text-center rounded-lg`}> Funcionário inativo</Text>
-
-
+            {isActive ? (
+                <PointGreen />
+            ) : (
+                <PointRed />
+            )}
+            <Text
+                style={tw`font-bold mb-2 bg-${isActive ? 'green' : 'red'}-200 
+            border-${isActive ? 'green' : 'red'}-600 border-2 p-2 w-4/8 text-center rounded-lg`}>
+                {isActive ? 'Funcionário ativo' : 'Funcionário inativo'}
+            </Text>
         </View>
-    )
+    );
 }
 
