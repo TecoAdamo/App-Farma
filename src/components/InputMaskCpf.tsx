557a3ce42b4f5f33edd30583607f54a89e27459c
Mateus Adamo <adamomateus478@gmail.com>
2024-02-27 15:20:34 -0300

    feat: Input mask ajusted.

--- src/components/InputMaskCpf.tsx
+++ src/components/InputMaskCpf.tsx
@@ -1,25 +1,54 @@
-import tw from 'twrnc'
+// import tw from 'twrnc'
 
-import MaskInput from 'react-native-mask-input'
+// import MaskInput from 'react-native-mask-input'
 
-const CPF_MASK = [/\d/, /\d/, /\d/, ".", /\d/, /\d/, /\d/, ".", /\d/, /\d/, /\d/, "-", /\d/, /\d/]
+// const CPF_MASK = [/\d/, /\d/, /\d/, ".", /\d/, /\d/, /\d/, ".", /\d/, /\d/, /\d/, "-", /\d/, /\d/]
+
+// interface InputCpfProps {
+//     value: string;
+//     onChangeText: (text: string) => void;
+// }
+
+// export default function InputCpf({ value, onChangeText }: InputCpfProps) {
+
+//     return (
+//         <MaskInput
+//             style={tw`border p-2 top-1/8 rounded w-full`}
+//             placeholder='ex: 123.456.789-10'
+//             value={value}
+//             onChangeText={onChangeText}
+//             mask={CPF_MASK}
+//             keyboardType='numeric'
+//         />
+//     );
+// }
+
+import React from 'react';
+import MaskInput from 'react-native-mask-input';
+import { ViewStyle } from 'react-native';  // Importando o tipo de estilo
+
+const CPF_MASK = [/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/];
 
 interface InputCpfProps {
     value: string;
     onChangeText: (text: string) => void;
+    placeholder?: string;
+    mask?: Array<RegExp>;
+    keyboardType?: string;
+    style?: ViewStyle;  // Adicionando a propriedade de estilo
 }
 
-export default function InputCpf({ value, onChangeText }: InputCpfProps) {
-
+const InputCpf: React.FC<InputCpfProps> = ({ value, onChangeText, placeholder, mask = CPF_MASK, keyboardType, style }) => {
     return (
         <MaskInput
-            style={tw`border p-2 top-1/8 rounded w-full`}
-            placeholder='ex: 123.456.789-10'
+            style={style}  // Usando o estilo fornecido como prop
+            placeholder={placeholder || 'ex: 123.456.789-10'}
             value={value}
             onChangeText={onChangeText}
-            mask={CPF_MASK}
+            mask={mask}
             keyboardType='numeric'
         />
     );
 }
 
+export default InputCpf;
