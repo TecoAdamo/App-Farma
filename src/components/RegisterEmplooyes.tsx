4379cdabf50404e350c23d9ddd6f3fa6276ccbf3
Mateus Adamo <adamomateus478@gmail.com>
2024-02-27 15:25:24 -0300

    feat: Button for register user and emplooye created.

--- src/components/RegisterEmplooyes.tsx
+++ src/components/RegisterEmplooyes.tsx
@@ -2,18 +2,29 @@ import { View, Text, TouchableOpacity } from 'react-native';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
 import tw from 'twrnc';
+import EmplooyeModal from './EmplooyeModal';
+import { useState } from 'react';
 
 export default function RegisterEmplooye() {
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const toggleModal = () => {
+        setModalVisible(!isModalVisible);
+    };
+
     return (
         <View style={tw` items-center top-1/8`}>
 
-            <View style={tw`top-1/8 right-1/8 p-1 w-full rounded gap-4`}>
+            <View style={tw`top-1/8 right-1/6 p-1 w-full rounded gap-4`}>
                 <TouchableOpacity
-                    style={tw`flex-row items-center bg-gray-900 p-2 rounded gap-4`}>
+                    style={tw`flex-row items-center bg-gray-900 p-2 rounded gap-4`}
+                    onPress={toggleModal}
+                >
                     <Ionicons name="person-add-outline" size={32} color="white" />
                     <Text style={tw`font-bold right-1/8 text-white`}>Cadastrar Funcionários</Text>
                 </TouchableOpacity>
             </View>
+            <EmplooyeModal visible={isModalVisible} onClose={toggleModal} />
         </View>
     );
 }
--- src/components/RegisterUserBtn.tsx
+++ src/components/RegisterUserBtn.tsx
@@ -1,19 +1,29 @@
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-
 import Ionicons from '@expo/vector-icons/Ionicons';
 import tw from 'twrnc';
+import UserModal from './UserModal';
 
-export default function RegisterUser() {
+export default function RegisterUsern() {
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const toggleModal = () => {
+        setModalVisible(!isModalVisible);
+    };
+
     return (
-        <View style={tw` items-center top-1/8`}>
-
-            <View style={tw`top-1/8 right-1/8 p-1 w-full rounded gap-4`}>
+        <View style={tw`items-center top-1/8`}>
+            <View style={tw`top-1/8 right-1/5 p-1 w-full rounded gap-4`}>
                 <TouchableOpacity
-                    style={tw`flex-row items-center bg-gray-900 p-2 rounded gap-4`}>
+                    style={tw`flex-row items-center bg-gray-900 p-2 rounded gap-4`}
+                    onPress={toggleModal}
+                >
                     <Ionicons name="people-outline" size={32} color="white" />
                     <Text style={tw`font-bold right-1/8 text-white`}>Cadastrar Usuários</Text>
                 </TouchableOpacity>
             </View>
+            <UserModal visible={isModalVisible} onClose={toggleModal} />
         </View>
     );
 }
+
